ac2d513234cc7a28fe9c2a20966c85dff1d5bcb2
JoacoC5 <joacocarro05@gmail.com>
2023-11-24 18:52:59 -0300

    Views y Controller Tablero

---
+++ Controllers/TableroController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using tl2_tp10_2023_JoacoC5.Models;
+using tl2_tp10_2023_JoacoC5.Repository;
+
+namespace tl2_tp10_2023_JoacoC5.Controllers;
+
+public class TableroController : Controller
+{
+    private readonly ILogger<TableroController> _logger;
+    private ITableroRepository tableroRepository;
+
+    public TableroController(ILogger<TableroController> logger)
+    {
+        _logger = logger;
+        tableroRepository = new TableroRepository();
+    }
+
+    [HttpGet]
+    public IActionResult CrearTablero()
+    {
+        return View(new Tablero());
+    }
+
+    [HttpPost]
+    public IActionResult CrearTablero(Tablero tablero)
+    {
+        tableroRepository.CreateTablero(tablero);
+        return RedirectToAction("ListarTablero");
+    }
+
+    [HttpGet]
+    public IActionResult Listartablero()
+    {
+        return View(tableroRepository.GetAllTablero());
+    }
+
+    [HttpGet]
+    public IActionResult ModificarTablero(int IdBuscado)
+    {
+        return View(tableroRepository.GetTableroByUsuario(IdBuscado));
+    }
+
+    [HttpPost]
+    public IActionResult Modificartablero(Tablero tablero)
+    {
+        tableroRepository.UpdateTablero(tablero.Id, tablero);
+        return RedirectToAction("ListarTablero");
+    }
+
+    //FALTA DELETE
+}
